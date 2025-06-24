a2069afa4391ccb2fed91fe8deae834b063c96b1
Sri Darsan Sah <sridarsansah5907@gmail.com>
2025-06-24 14:20:44 +0530

    add missing tailwindcss dependencies and update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwidcss from 'tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwidcss()],
   base: '/BookMedico/', // <-- set this to your repo name if using a subfolder
 });
