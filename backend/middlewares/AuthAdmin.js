f41c22b44a79cbe065f37b0ccbaf81e5c97437c6
Sri Darsan Sah <sridarsansah5907@gmail.com>
2025-06-24 18:11:10 +0530

    add api for admin login and adding doctor by admin

---
+++ backend/middlewares/AuthAdmin.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken"
+
+// admin authentication middleware
+const authAdmin = async (req, res, next) => {
+    try {
+        const {atoken} = req.headers
+        if (!atoken) {
+            return res.json({success:false, message:"Authentication token is missing"})
+        }
+        const token_decode = jwt.verify(atoken, process.env.JWT_SECRET)
+        if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+            return res.json({success:false, message:"Invalid authentication token"})
+        }
+        next()
+    } catch (error) {
+        console.log(error) 
+        res.json({success:false, message:"Authentication failed"})
+    }
+}
+
+export default authAdmin
