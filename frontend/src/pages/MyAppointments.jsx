2b2927806a28d9e4bfdb9050bb4c59445942aff6
Sri Darsan Sah <sridarsansah5907@gmail.com>
2025-07-27 13:39:01 +0530

    bug fixed: don't show payment gateway for already paid appointments

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -122,7 +122,7 @@ const MyAppointments = () => {
               </div>
               <div className='flex flex-col gap-2 justify-end'>
                 { !item.cancelled && item.payment && !item.isCompleted &&
-                  <button onClick={() => appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded bg-gray-300 hover:text-white transition-all duration-300'>Paid</button>
+                  <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded bg-gray-300 hover:text-white transition-all duration-300'>Paid</button>
                 }
                 { !item.cancelled && !item.payment && !item.isCompleted &&
                   <button onClick={() => appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>
